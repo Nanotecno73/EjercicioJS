f4d04cfa45b263b40620b6fe9e46a4a2f477fe02
Nanotecno73 <nanotecno73@github.com>
2023-06-06 18:24:17 -0300

    subiendo ejercicioJS

---
+++ ejercicio_js.js
@@ -0,0 +1,503 @@
+//Objetos Literales
+
+//Defino estructura del objeto (sus atributos y métodos)
+
+let celular={
+    id: 000,
+    marca: "",
+    modelo: "",
+    ram: 00,
+    rom: 00,
+    color: "",
+
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar?: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar?: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+
+}
+
+console.log("Estructura del objeto definida")
+
+//Defino el array de objetos:
+
+let celulares = [
+    {
+    id: 001,
+    marca: "Motorola",
+    modelo: "G20",
+    ram: 2,
+    rom: 32,
+    color: "negro",
+
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+},
+
+ {
+    id: 002,
+    marca: "Motorola",
+    modelo: "G30",
+    ram: 2,
+    rom: 64,
+    color: "azul",
+    
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+},
+
+{
+    id: 003,
+    marca: "Motorola",
+    modelo: "G40",
+    ram: 3,
+    rom: 64,
+    color: "negro",
+    
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+},
+
+{
+    id: 004,
+    marca: "Motorola",
+    modelo: "G50",
+    ram: 4,
+    rom: 128,
+    color: "negro",
+    
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+},
+
+{
+    id: 005,
+    marca: "Motorola",
+    modelo: "E10",
+    ram: 2,
+    rom: 32,
+    color: "negro",
+    
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+},
+
+{
+    id: 006,
+    marca: "Motorola",
+    modelo: "E20",
+    ram: 2,
+    rom: 32,
+    color: "gris",
+    
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+},
+
+{
+    id: 010,
+    marca: "Samsung",
+    modelo: "A10",
+    ram: 2,
+    rom: 32,
+    color: "negro",
+    
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+},
+
+{
+    id: 011,
+    marca: "Samsung",
+    modelo: "A20",
+    ram: 2,
+    rom: 64,
+    color: "gris",
+    
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+},
+
+{
+    id: 012,
+    marca: "Samsung",
+    modelo: "A30",
+    ram: 3,
+    rom: 64,
+    color: "blanco",
+    
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+},
+
+{
+    id: 013,
+    marca: "Samsung",
+    modelo: "A50",
+    ram: 4,
+    rom: 128,
+    color: "azul",
+    
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+},
+
+{
+    id: 014,
+    marca: "Samsung",
+    modelo: "A70",
+    ram: 6,
+    rom: 128,
+    color: "negro",
+    
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+},
+
+{
+    id: 015,
+    marca: "Samsung",
+    modelo: "S10",
+    ram: 6,
+    rom: 128,
+    color: "negro",
+    
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+},
+
+{
+    id: 016,
+    marca: "Samsung",
+    modelo: "S20",
+    ram: 6,
+    rom: 256,
+    color: "plata",
+    
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+},
+
+{
+    id: 020,
+    marca: "Iphone",
+    modelo: "8",
+    ram: 4,
+    rom: 64,
+    color: "negro",
+    
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+},
+
+{
+    id: 021,
+    marca: "Iphone",
+    modelo: "10",
+    ram: 4,
+    rom: 128,
+    color: "negro",
+    
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+},
+
+{
+    id: 022,
+    marca: "Iphone",
+    modelo: "11",
+    ram: 6,
+    rom: 256,
+    color: "negro",
+    
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+},
+
+{
+    id: 023,
+    marca: "Iphone",
+    modelo: "13",
+    ram: 6,
+    rom: 256,
+    color: "negro",
+    
+    ingresar: function(){
+        console.log("Ingrese Marca")
+        console.log("Ingrese Modelo")
+        console.log("Ingrese cantidad de memoria RAM")
+        console.log("Ingrese cantidad de memoria ROM")
+        console.log("Ingrese Color")
+    },
+
+    modificar: function(){
+        console.log("Qué desea modificar: 1-Marca, 2-Modelo, 3-Mem RAM, 4-Mem ROM, 5-Color")
+
+    },
+
+    eliminar: function(){
+        console.log("Qué desea eliminar: 1-Id, 2-Marca, 3-Modelo")
+    }
+
+}
+
+
+]
+
+console.log("Array de objetos definido")
+
+
+
